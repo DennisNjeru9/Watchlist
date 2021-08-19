eaaa374c0de3f56e0cffceae4003eeff62e84c34
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-19 18:29:06 +0300

    create test to establish whether movie objects are instantiated correctly.

--- app/models/movie.py
+++ app/models/movie.py
@@ -3,10 +3,24 @@ class Movie:
     Movie class to define Movie Objects.
     '''
 
-    def __init__(self, id, title, overview, poster, vote_average, vote_count):
+    def __init__(self,id,title,overview,poster,vote_average,vote_count):
+        '''
+        Create an init method to allow the passing of parameters needed inside the movie objects.
+
+        Args:
+            id = The movie id.
+            title = The movie's name
+            overview = brief description of the movie
+            poster = poster image of the movie
+            vote_average = average rating of the movie
+            vote_count = votes involved in rating the movie
+        '''
         self.id = id
         self.title = title
         self.overview = overview
         self.poster = poster
         self.vote_average = vote_average
         self.vote_count = vote_count
+
+
+        pass
